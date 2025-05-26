f9547d845afa2fa7f0f4e884dec429f620e35aeb
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-05-26 12:33:40 -0500

    Watch list ready

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,24 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
   end
 
   def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,12 +3,27 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-  # def show
-  # end
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
